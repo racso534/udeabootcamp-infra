34b23d1e135eb9da17ef4476cc6326e6afed25af
Oscar Rivero <racso534@hotmail.com>
2025-08-10 10:24:42 -0500

    adding script

---
+++ scripts/deploy.bash
@@ -0,0 +1,17 @@
+#!/bin/bash
+set -e
+
+STACK_NAME=$1
+TEMPLATE_FILE=$2
+REGION="us-east-1"  # Ajusta según tu necesidad
+
+if [ -z "$STACK_NAME" ] || [ -z "$TEMPLATE_FILE" ]; then
+    echo "Uso: ./deploy.sh <stack-name> <template-file>"
+    exit 1
+fi
+
+aws cloudformation deploy \
+  --stack-name "$STACK_NAME" \
+  --template-file "$TEMPLATE_FILE" \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --region "$REGION"
