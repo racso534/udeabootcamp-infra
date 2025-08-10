4c7692eb581bb60e78189eccc86dc2b5433712dd
Oscar Rivero <racso534@hotmail.com>
2025-08-10 13:00:38 -0500

    adding permissions

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,7 +3,7 @@ set -e
 
 STACK_NAME=$1
 TEMPLATE_FILE=$2
-REGION="us-east-2"  # Ajusta según tu necesidad
+REGION="us-east-2"
 
 if [ -z "$STACK_NAME" ] || [ -z "$TEMPLATE_FILE" ]; then
     echo "Uso: ./deploy.sh <stack-name> <template-file>"
